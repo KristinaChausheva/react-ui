2e2d621708ce49555d152a627d1cb158525ec5d6
Kristina Chausheva <k.chausheva@gmail.com>
2022-03-23 21:35:43 +0200

    adding router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Explore from "./pages/Explore"
+import Template from "./pages/Template"
+
 function App() {
-  return <div className="App">hjhjb</div>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/template" element={<Template />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App
